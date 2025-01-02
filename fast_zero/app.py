50fde76bfabb099f983fb04460b8f4c41cc58f48
Potatoyz908 <euller2005@gmail.com>
2025-01-02 09:28:48 -0300

    feat(api): adiciona rota para listar usuários e refatora a criação de usuários

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -2,12 +2,12 @@ from http import HTTPStatus
 
 from fastapi import FastAPI
 
-from fast_zero.schemas import Message, UserSchema, UserPublic, UserDB
-
+from fast_zero.schemas import Message, UserDB, UserPublic, UserSchema
 
 app = FastAPI()
 
 database = []
+
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read__root():
@@ -16,7 +16,11 @@ def read__root():
 
 @app.post('/users', status_code=HTTPStatus.CREATED, response_model=UserPublic)
 def create_user(user: UserSchema):
-
     user_with_id = UserDB(id=len(database) + 1, **user.model_dump())
     database.append(user_with_id)
     return user_with_id
+
+
+@app.get('/users/', response_model=dict)
+def read_users():
+    return {'users': database}
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,28 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fast_zero.app import app
-
-
-def test_read_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app)  # Arrange (configuração)
-
+def test_read_root_deve_retornar_ok_e_ola_mundo(client):
     response = client.get('/')  # Act (ação)
 
     assert response.status_code == HTTPStatus.OK  # Assert (verificação)
     assert response.json() == {'message': 'Hello, World!'}  # Assert
+
+
+def test_create_user(client):
+    response = client.post(
+        '/users',
+        json={
+            'username': 'batatinha123',
+            'password': 'password',
+            'email': 'test@test.com',
+        },
+    )
+
+    # validar o status code da resposta
+    assert response.status_code == HTTPStatus.CREATED
+    # validar UserPublic
+    assert response.json() == {
+        'id': 1,
+        'username': 'batatinha123',
+        'email': 'test@test.com',
+    }
