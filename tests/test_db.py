e0184628c4e1079e986a41da7bc99f9180c4ff76
Potatoyz908 <euller2005@gmail.com>
2025-01-02 11:41:07 -0300

    feat(tests): atualiza teste de criação de usuário para usar banco de dados em memória

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,8 +1,22 @@
-from fast_zero.models import User
+from sqlalchemy import create_engine, select
+from sqlalchemy.orm import Session
+
+from fast_zero.models import User, table_registry
 
 
 def test_create_user(client):
-    user = User(
-        username='batatinha123', email='test@test.com', password='password'
-    )
-    assert user.username == 'batatinha123'
+    engine = create_engine('sqlite:///:memory:')
+
+    table_registry.metadata.create_all(engine)
+
+    with Session(engine) as session:
+        user = User(
+            username='batatinha123', email='test@test.com', password='password'
+        )
+        session.add(user)
+        session.commit()
+        result = session.scalar(
+            select(User).where(User.email == 'test@test.com')
+        )
+
+    assert result.username == 'batatinha123'
