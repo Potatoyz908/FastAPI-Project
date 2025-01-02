2c8de9ef0b568476de29861a36f940fc75d2ebab
Potatoyz908 <euller2005@gmail.com>
2025-01-02 09:00:14 -0300

    feat(api): atualiza a rota de criação de usuários para retornar o usuário com ID e armazena no banco de dados

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -2,7 +2,8 @@ from http import HTTPStatus
 
 from fastapi import FastAPI
 
-from fast_zero.schemas import Message, UserSchema, UserPublic
+from fast_zero.schemas import Message, UserSchema, UserPublic, UserDB
+
 
 app = FastAPI()
 
@@ -15,5 +16,7 @@ def read__root():
 
 @app.post('/users', status_code=HTTPStatus.CREATED, response_model=UserPublic)
 def create_user(user: UserSchema):
-    
-    return user
+
+    user_with_id = UserDB(id=len(database) + 1, **user.model_dump())
+    database.append(user_with_id)
+    return user_with_id
